e916a539faa388b14c7c5d054fa2c3497bbf9787
nayVoo <124888494+nayVoo@users.noreply.github.com>
2024-01-19 14:25:05 -0500

    cryptobot.py

--- My crypto bot/my_cryptobot.py
+++ My crypto bot/my_cryptobot.py
@@ -1,4 +1,4 @@
-import keys
+#import keys
 import aiohttp
 import asyncio
 import threading
@@ -22,13 +22,16 @@ test_api_secret = os.getenv('test_api_secret')
 
 
 
-
 start = datetime.now()
 client = Client(test_api_key, test_api_secret, testnet=True) #testnet = fake trade platform
 
 token_list = []
 futures_list = []
-delete_list = ['EOSETH', 'SNTETH']
+sort_list = ['BTC', 'ETH', 'ETHBTC', 'LTCBTC', 'BNBBTC', 'BTCUSDT', 'ETHUSDT', 'TRXBTC', 'XRPBTC', 'BNBUSDT',
+	'LTCUSDT','LTCBNB', 'XRPUSDT', 'XRPBNB', 'TRXBNB', 'TRXUSDT', 'NEOBTC', 'QTUMETH', 'EOSETH', 'SNTETH', 'BNTETH',
+	'GASBTC', 'BNBETH', 'LRCBTC', 'LRCETH', 'QTUMBTC', 'ZRXBTC', 'KNCBTC', 'FUNETH', 'NEOETH', 'IOTABTC', 'IOTAETH',
+	'LINKBTC', 'LINKETH', 'XVGETH', 'MTLBTC', 'EOSBTC', 'SNTBTC', 'ETCETH', 'ETCBTC', 'ZECBTC', 'ZECETH', 'BNTBTC',
+	'ASTBTC', 'DASHBTC', 'DASHETH', 'OAXBTC', 'REQBTC', 'VIBBTC', 'TRXETH', 'POWRBTC', 'POWRETH', 'XRPETH', 'ENJBTC']
 
 df_columns = [] # Для столпцов в удобном для меня виде
 extremum_dict = {} #Для списка highc, lowc, highf, lowf, openc, UTC, сюда довольное быстро производиться запись
@@ -36,11 +39,19 @@ volume_dict = {} # Для токенов
 
 
 #Список токенов
-def get_token():
+def get_sort_token():
 	global token_list
 	spot_info = client.get_exchange_info()
 	spot_list = [symbol['symbol'] for symbol in spot_info['symbols'] if symbol.get('permissions', [])[0] == 'SPOT']
 	token_list.extend(spot_list)
+	#print(token_list)
+	#Сюда кидаю монеты и они сортируються, потом сделать так что бы ещё и delete_lsit кидался в дальнейшем
+	to_sort = [x for x in token_list if x in sort_list]
+	token_list = to_sort
+	return token_list
+
+
+
 
 #Список фючерсов
 def get_futures():
@@ -49,47 +60,98 @@ def get_futures():
 	contact_list = [contract['symbol'] for contract in contact_info['symbols'] if contract.get('contractType', None) == 'PERPETUAL'] 
 	futures_list.extend(contact_list)
 
-#Сюда из другой функции кидаю монеты и они сортируються, потом сделать так что бы ещё и delete_lsit кидался в дальнейшем
-def sort_token(to_sort):
-	to_sort = [x for x in token_list if x not in delete_list]
-	return to_sort
+
 
 #Получаем свечи и их значения
-def get_klines(symbol_list):
-	global df_columns
-	global extremum_dict
-	global volume_dict
-	df = pd.DataFrame(columns=symbol_list, index=['highc', 'lowc', 'highf', 'lowf', 'openc', 'UTC'])
-	#print(df)
+#def get_klines(symbol_list):
+#	global df_columns
+#	global extremum_dict
+#	global volume_dict
+#	df = pd.DataFrame(columns=symbol_list, index=['highc', 'lowc', 'highf', 'lowf', 'openc', 'UTC'])
+#	print(df)
+#
+#	for symbol in symbol_list:
+#		klines = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1DAY, limit=5)
+#		try:
+#			openc = float(klines[-1][1])
+#			highc = float(klines[-1][2])
+#			lowc = float(klines[-1][3])
+#			highf = float(klines[0][2])
+#			lowf = float(klines[0][3])
+#			UTC = formatted_time = datetime.utcfromtimestamp(float(klines[-1][0]) / 1000).strftime('%Y-%m-%d %H:%M:%S')
+#		except Exception as ex: print(f'Exception in get_klines: {ex}')
+#		df.loc['highc', symbol] = highc # Цена верхней свечи сейчас
+#		df.loc['lowc', symbol] = lowc # Цена нижней свечи сейчас
+#		df.loc['highf', symbol] = highf # Цена верхней нижней свечи
+#		df.loc['lowf', symbol] = lowf # Цена первой нижней свечи
+#		df.loc['openc', symbol] = openc # Цена текущего открытия
+#		df.loc['UTC', symbol] = UTC # Время открытия
+#		volume_dict[symbol] = symbol
+#
+#
+#	df = df.T #строки в столбцы or столбцы в строки
+#	df_columns = df
+#	df_dict = df.to_dict()
+#	print('data dicttt----------------------------')
+#	for symbol, value_dict in df_dict.items():
+#		for key in value_dict.keys(): #symbol - назва столбца (highc,lowc,countc...), в value_dict дохера инфы поэтому ещё один цикл
+#			extremum_dict[key] = {
+#			'highc': df_dict['highc'][key], 'lowc': df_dict['lowc'][key], 
+#			'highf': df_dict['highf'][key], 'lowf': df_dict['lowf'][key], 
+#			'openc': df_dict['openc'][key], 'UTC': df_dict['UTC'][key],
+#			}
+#
+#	print(extremum_dict)
+#
+		##		ASYNC VERSION OF KLINES			##
+async def get_klines(symbol_list):
+    global df_columns
+    global extremum_dict
+    global volume_dict
 
-	for symbol in symbol_list:
-		klines = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1DAY, limit=5)
-		try:
-			openc = float(klines[-1][1])
-			highc = float(klines[-1][2])
-			lowc = float(klines[-1][3])
-			highf = float(klines[0][2])
-			lowf = float(klines[0][3])
-			UTC = formatted_time = datetime.utcfromtimestamp(float(klines[-1][0]) / 1000).strftime('%Y-%m-%d %H:%M:%S')
-		except Exception as ex: print(f'Exception in get_klines: {ex}')
-		df.loc['highc', symbol] = highc # Цена верхней свечи сейчас
-		df.loc['lowc', symbol] = lowc # Цена нижней свечи сейчас
-		df.loc['highf', symbol] = highf # Цена верхней нижней свечи
-		df.loc['lowf', symbol] = lowf # Цена первой нижней свечи
-		df.loc['openc', symbol] = openc # Цена текущего открытия
-		df.loc['UTC', symbol] = UTC # Время открытия
-		volume_dict[symbol] = symbol
-	df = df.T #строки в столбцы or столбцы в строки
-	df_columns = df
-	df_dict = df.to_dict()
-	print('data dicttt----------------------------')
-	for symbol, value_dict in df_dict.items():
-		for key in value_dict.keys(): #symbol - назва столбца (highc,lowc,countc...), в value_dict дохера инфы поэтому ещё один цикл
-			extremum_dict[key] = {
-			'highc': df_dict['highc'][key], 'lowc': df_dict['lowc'][key], 
-			'highf': df_dict['highf'][key], 'lowf': df_dict['lowf'][key], 
-			'openc': df_dict['openc'][key], 'UTC': df_dict['UTC'][key],
-			}
+    # Construct DataFrame without await
+    df = pd.DataFrame(columns=symbol_list, index=['highc', 'lowc', 'highf', 'lowf', 'openc', 'UTC'])
+    print(df)
+
+    for symbol in symbol_list:
+        klines = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1DAY, limit=5)
+        try:
+            openc = float(klines[-1][1])
+            highc = float(klines[-1][2])
+            lowc = float(klines[-1][3])
+            highf = float(klines[0][2])
+            lowf = float(klines[0][3])
+            timestamp = float(klines[-1][0]) / 1000
+            UTC = formatted_time = datetime.utcfromtimestamp(float(klines[-1][0]) / 1000).strftime('%Y-%m-%d %H:%M:%S')
+        except Exception as ex:
+            print(f'Exception in get_klines: {ex}')
+
+        df.loc['highc', symbol] = highc
+        df.loc['lowc', symbol] = lowc
+        df.loc['highf', symbol] = highf
+        df.loc['lowf', symbol] = lowf
+        df.loc['openc', symbol] = openc
+        df.loc['UTC', symbol] = UTC
+        volume_dict[symbol] = symbol
+
+    df = df.T  # строки в столбцы or столбцы в строки
+    df_columns = df
+    df_dict = df.to_dict()
+    print(df)
+    print('data dicttt----------------------------')
+    for symbol, value_dict in df_dict.items():
+        for key in value_dict.keys():
+            extremum_dict[key] = {
+                'highc': df_dict['highc'][key], 'lowc': df_dict['lowc'][key],
+                'highf': df_dict['highf'][key], 'lowf': df_dict['lowf'][key],
+                'openc': df_dict['openc'][key], 'UTC': df_dict['UTC'][key],
+            }
+
+    #print(extremum_dict)
+
+
+
+
 
 async def socket_connect(symbol_list):
 	global volume_dict
@@ -168,10 +230,9 @@ def compare_price_tokens(price, symbol, volume):#(symbol, open_price, close_pric
 #ПРОВЕЛ ТЕСТ, ОШИБОК НЕТУ
 
 
+get_sort_token()
+asyncio.run(get_klines(token_list))
 
-
-get_token()
-get_klines(token_list)
 
 async def main():
     await socket_connect(token_list)
